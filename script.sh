573e1fbc119411b8b65768f74592b0f332472691
Sagar-Sagar2002 <sagar672002@gmail.com>
2025-08-08 11:39:33 +0000

    script.sh

---
+++ script.sh
@@ -0,0 +1,2 @@
+
+Linux commands are instructions given to the Linux operating system through a command-line interface (CLI), also known as the terminal or shell. These commands allow users to interact with the system, manage files and directories, control processes, configure settings, and perform various administrative tasks.
